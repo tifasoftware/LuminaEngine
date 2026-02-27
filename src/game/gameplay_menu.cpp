c1ea486305a5379f6c412351dcd1b622df5dfabd
Infilit <tifasoftware@gmx.com>
2026-02-27 09:38:03 -0600

    Split gameplay.cpp

---
+++ src/game/gameplay_menu.cpp
@@ -0,0 +1,58 @@
+#include <game/gameplay.h>
+
+void GamePlay::MenuDraw()
+{
+    if (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    // End the loop if the programs is being closed
+                    gameState = EXIT;
+                    break;
+                case SDL_CONTROLLERDEVICEADDED:
+                    // Connect a controller when it is connected
+                    SDL_GameControllerOpen(event.cdevice.which);
+                    break;
+                case SDL_CONTROLLERBUTTONDOWN:
+                    if(event.cbutton.button == SDL_CONTROLLER_BUTTON_START) {
+                        // Close the program if start is pressed
+                        snd->playSFX(chime);
+                    } 
+                    else if (event.cbutton.button == SDL_CONTROLLER_BUTTON_DPAD_UP) {
+                        luminaMoveY = -1 * luminaMoveRate;
+                    }
+                    else if (event.cbutton.button == SDL_CONTROLLER_BUTTON_DPAD_DOWN) {
+                        luminaMoveY = luminaMoveRate;
+                    }
+                    else if (event.cbutton.button == SDL_CONTROLLER_BUTTON_DPAD_LEFT) {
+                        luminaMoveX = -1 * luminaMoveRate;
+                    }
+                    else if (event.cbutton.button == SDL_CONTROLLER_BUTTON_DPAD_RIGHT) {
+                        luminaMoveX = luminaMoveRate;
+                    }
+                    break;
+                case SDL_CONTROLLERBUTTONUP:
+                    if (event.cbutton.button == SDL_CONTROLLER_BUTTON_DPAD_UP) {
+                        luminaMoveY = 0;
+                    }
+                    else if (event.cbutton.button == SDL_CONTROLLER_BUTTON_DPAD_DOWN) {
+                        luminaMoveY = 0;
+                    }
+                    else if (event.cbutton.button == SDL_CONTROLLER_BUTTON_DPAD_LEFT) {
+                        luminaMoveX = 0;
+                    }
+                    else if (event.cbutton.button == SDL_CONTROLLER_BUTTON_DPAD_RIGHT) {
+                        luminaMoveX = 0;
+                    }
+                    break;
+            }
+        }
+
+        // Clear the screen
+        r->clear();
+
+        text->Render(r);
+        // Draw everything on a white background
+        
+        r->present();
+        
+}
