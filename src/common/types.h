67e4067f2bfde1c69f785b42e160afe33b32cf2f
Infilit <tifasoftware@gmx.com>
2026-03-15 21:54:28 -0500

    Start groundwork for incorporating entites in LMAPs

--- src/common/types.h
+++ src/common/types.h
@@ -18,11 +18,30 @@ enum CollisionType : uint8_t
     SWI
 };
 
-enum GameState
+enum GameState : uint8_t
 {
     WORLD,
     BATTLE,
     MENU,
     EXIT,
     TRANSITION
+};
+
+enum EntityType : uint8_t
+{
+    TRIGGER_LUA,
+    TRIGGER_WARP,
+    TRIGGER_BATTLEFIELD,
+    INTERACT_NPC,
+    INTERACT_LUA,
+    OBSTACLE
+};
+
+struct Entity {
+    const char* name;
+    EntityType type;
+    int x;
+    int y;
+    int width;
+    int height;
 };
