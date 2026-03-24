ffb6fbb1064d9445b1eb74df51fb54d72d6df663
Infilit <tifasoftware@gmx.com>
2026-03-23 19:26:40 -0500

    Create macOS build script

---
+++ build_macos.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+cmake .. -DCMAKE_OSX_ARCHITECTURES=arm64 -B build-arm64
+cmake .. -DCMAKE_OSX_ARCHITECTURES=x86_64 -B build-x86_64
+
+cmake --build build-arm64
+cmake --build build-x86_64
+
+lipo -create build-arm64/LuminaEngine build-x86_64/LuminaEngine LuminaEngine-universal
