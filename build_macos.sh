c0f559b8c6eb2fb132ee93b68d3775cc67896bc2
Infilit <tifasoftware@gmx.com>
2026-03-23 20:19:35 -0500

    experimentation to get it builing on macOS

--- build_macos.sh
+++ build_macos.sh
@@ -6,4 +6,4 @@ cmake .. -DCMAKE_OSX_ARCHITECTURES=x86_64 -B build-x86_64
 cmake --build build-arm64
 cmake --build build-x86_64
 
-lipo -create build-arm64/LuminaEngine build-x86_64/LuminaEngine LuminaEngine-universal
+lipo -create build-arm64/LuminaEngine build-x86_64/LuminaEngine -output LuminaEngine-universal
