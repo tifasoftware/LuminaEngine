edd183320283169353baa967e978afa3e3b25ae0
Infilit <tifasoftware@gmx.com>
2026-03-25 12:32:06 -0500

    Deleted remnants from old polymorphic entitiy system

--- src/lumina.h
+++ src/lumina.h
@@ -2,17 +2,25 @@
 
 #include <graphics/Renderer.h>
 #include <graphics/Texture.h>
-#include <entities/TileMap.h>
 #include <graphics/effects/fade.h>
+#include <entities/TileMap.h>
 #include <entities/character.h>
+#include <entities/entity.h>
 #include <common/types.h>
+#include <common/utils.h>
+#include <common/vector.h>
 #include <sound/soundsystem.h>
 #include <vgui/text.h>
 #include <vgui/panel.h>
 #include <vgui/UIElement.h>
 #include <vgui/button.h>
+#include <vgui/dialogue.h>
 #include <game/gameplay.h>
+#include <game/controller.h>
+#include <game/gameplay_state.h>
+#include <game/IControllable.h>
 #include <platform/platform.h>
+#include <lua.h>
 
 #ifdef PLATFORM_OSX
 #include <mach-o/dyld.h>
