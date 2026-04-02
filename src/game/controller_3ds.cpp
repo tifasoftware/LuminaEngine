f1a0e2caadb77bd64831cad9d394441bb5498473
InfiTechPPC <tifasoftware@gmx.com>
2026-04-02 12:51:21 -0500

    switch to native libraries for input processing on Nintendo 3DS

--- src/game/controller_3ds.cpp
+++ src/game/controller_3ds.cpp
@@ -3,53 +3,54 @@
 
 #include "controller.h"
 
-void Controller::N3DS_ProcessInput() {
-    if (SDL_PollEvent(&event)) {
-        switch (event.type) {
-            case SDL_QUIT:
-                pawn->OnQuit();
-                break;
-            case SDL_KEYDOWN:
-                if(event.key.keysym.sym == SDLK_RETURN) {
-                    pawn->OnButtonStart();
-                } else if (event.key.keysym.sym == SDLK_ESCAPE) {
-                    pawn->OnButtonSelect();
-                } else if (event.key.keysym.sym == SDLK_a) {
-                    pawn->OnButtonA();
-                } else if (event.key.keysym.sym == SDLK_b) {
-                    pawn->OnButtonB();
-                } else if (event.key.keysym.sym == SDLK_x) {
-                    pawn->OnButtonX();
-                } else if (event.key.keysym.sym == SDLK_y) {
-                    pawn->OnButtonY();
-                } else if (event.key.keysym.sym == SDLK_UP) {
-                    pawn->OnButtonUp();
-                } else if (event.key.keysym.sym == SDLK_DOWN) {
-                    pawn->OnButtonDown();
-                } else if (event.key.keysym.sym == SDLK_LEFT) {
-                    pawn->OnButtonLeft();
-                } else if (event.key.keysym.sym == SDLK_RIGHT) {
-                    pawn->OnButtonRight();
-                }
-                break;
-            default:
-                break;
-        }
-    }
-    const Uint8* keys = SDL_GetKeyboardState(NULL);
-    if (keys[SDL_SCANCODE_UP]) {
-        pawn->OnMoveUp();
-    } else if (keys[SDL_SCANCODE_DOWN]) {
-        pawn->OnMoveDown();
-    } else {
-        pawn->OnStopMoveDown();
-    }
-    if (keys[SDL_SCANCODE_LEFT]) {
-        pawn->OnMoveLeft();
-    } else if (keys[SDL_SCANCODE_RIGHT]) {
-        pawn->OnMoveRight();
-    } else {
-        pawn->OnStopMoveRight();
-    }
+#if defined(PLATFORM_3DS) || defined(LIB_SDL1)
+#include <3ds.h>
 
+void Controller::N3DS_ProcessInput() {
+    hidScanInput();
+
+    uint32_t kDown = hidKeysDown();
+    uint32_t kHeld = hidKeysHeld();
+    uint32_t kUp = hidKeysUp();
+
+    if (kDown && KEY_START) {
+        pawn->OnButtonStart();
+    } else if (kDown && KEY_SELECT) {
+        pawn->OnButtonSelect();
+    } else if (kDown && KEY_UP) {
+        pawn->OnButtonUp();
+        pawn->OnMoveUp();
+    } else if (kDown && KEY_DOWN) {
+        pawn->OnButtonDown();
+        pawn->OnMoveDown();
+    } else if (kDown && KEY_LEFT) {
+        pawn->OnButtonLeft();
+        pawn->OnMoveLeft();
+    } else if (kDown && KEY_RIGHT) {
+        pawn->OnButtonRight();
+        pawn->OnMoveRight();
+    } else if (kUp && KEY_UP) {
+        pawn->OnStopMoveUp();
+    } else if (kUp && KEY_DOWN) {
+        pawn->OnStopMoveDown();
+    } else if (kUp && KEY_LEFT) {
+        pawn->OnStopMoveLeft();
+    } else if (kUp && KEY_RIGHT) {
+        pawn->OnStopMoveRight();
+    } else if (kDown && KEY_A) {
+        pawn->OnButtonA();
+    } else if (kDown && KEY_B) {
+        pawn->OnButtonB();
+    } else if (kDown && KEY_X) {
+        pawn->OnButtonX();
+    } else if (kDown && KEY_Y) {
+        pawn->OnButtonY();
+    }
 }
+#else
+
+void N3DS_ProcessInput() {
+    //no-ops
+}
+
+#endif
