59bd242a55037a42693d5fae8bd2f7e8d25779eb
orbisai0security <mediratta01.pally@gmail.com>
2026-05-22 02:24:55 +0000

    fix: utils.custom.buffer-overflow-strcpy security vulnerability
    Automated security fix generated by OrbisAI Security

--- libs/lua-5.5.0/src/luac.c
+++ libs/lua-5.5.0/src/luac.c
@@ -309,7 +309,7 @@ static void PrintConstant(const Proto* f, int i)
   case LUA_VNUMFLT:
 	{
 	char buff[100];
-	sprintf(buff,LUA_NUMBER_FMT,fltvalue(o));
+	snprintf(buff,sizeof(buff),LUA_NUMBER_FMT,fltvalue(o));
 	printf("%s",buff);
 	if (buff[strspn(buff,"-0123456789")]=='\0') printf(".0");
 	break;
