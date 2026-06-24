c774f3b08c3715239fa3ccaf5bddea3a74b9320a
Infilit <tifasoftware@gmx.com>
2026-06-23 22:29:21 -0500

    Yang: Added more includes to ps2.h

--- src/platform/ps2.h
+++ src/platform/ps2.h
@@ -2,7 +2,11 @@
 
 #ifdef PLATFORM_PS2
 
+#include <kernel.h>
 #include <ps2_all_drivers.h>
+#include <sifrpc.h>
+#include <iopcontrol.h>
+#include <sbv_patches.h>
 
 static void _reset_IOP(void) {
     SifInitRpc(0);
