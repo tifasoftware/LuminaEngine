ae460ebcb3b1cc876b8e4861e5792a16390a8fa4
Infilit <tifasoftware@gmx.com>
2026-06-30 16:03:29 -0500

    First invokation of LE code on Android

--- Android/fetch-sdl.sh
+++ Android/fetch-sdl.sh
@@ -2,8 +2,27 @@
 
 cd app/jni
 
-git clone -b SDL2 https://github.com/libsdl-org/SDL.git
+git clone -b release-2.26.x https://github.com/libsdl-org/SDL.git
 git clone -b SDL2 https://github.com/libsdl-org/SDL_image.git
 git clone -b SDL2 https://github.com/libsdl-org/SDL_mixer.git
+
+cd SDL
+\rm -rf .git
+cd ..
+cd SDL_image
+\rm -rf .git
+cd external
+./download.sh
+cd ../..
+cd SDL_mixer
+\rm -rf .git
+cd external
+./download.sh
+cd ../..
+
+mkdir include
+cp -r SDL/include include/SDL2
+cp SDL_image/include/SDL_image.h include/SDL2/SDL_image.h
+cp SDL_mixer/include/SDL_mixer.h include/SDL2/SDL_mixer.h
 
 cd ../..
