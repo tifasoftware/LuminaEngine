7ef4782d35bf804d38f8b207f02214581c4d45b8
Infilit <tifasoftware@gmx.com>
2026-07-01 23:25:10 -0500

    Implement Android Adaptive Display Algorithm

--- src/platform/mobile/mdisplay.h
+++ src/platform/mobile/mdisplay.h
@@ -1,25 +1,63 @@
 #pragma once
 
+#include <SDL2/SDL.h>
+
+struct MobileResolution {
+    int height = 0;
+    int width = 0;
+};
+
 class MobileDisplay {
 public:
     static inline int GetScreenHeight()
     {
-        return 600;
+        auto dm = GetDM();
+        return dm.height;
     }
     static inline int GetScreenWidth()
     {
-        return 800;
+        auto dm = GetDM();
+        return dm.width;
     }
     static inline int GetDrawHeight()
     {
-        return GetScreenHeight();
+        return GetScreenHeight() / GetRecommendedScale();
     }
     static inline int GetDrawWidth()
     {
-        return GetScreenWidth();
+        return GetScreenWidth() / GetRecommendedScale();
     }
     static inline int GetRecommendedScale()
     {
-        return 1;
+        auto dm = GetDM();
+
+        int s = 1;
+        int w = dm.width;
+        int h = dm.height;
+
+        while ((w > 480) || (h > 272)) {
+            w /= 2;
+            h /= 2;
+            s++;
+        }
+        return s;
     }
+
+private:
+    static inline MobileResolution GetDM()
+    {
+        MobileResolution mr;
+        SDL_DisplayMode dm;
+        if (SDL_GetCurrentDisplayMode(0, &dm) == 0)
+        {
+            mr.height = dm.h;
+            mr.width = dm.w;
+        } else {
+            mr.height = 600;
+            mr.width = 800;
+        }
+        return mr;
+    }
+
+
 };
